346e0615018a8a88ab885640cfe833fc0e14cb6e
Bozhinovski15 <p.bozinovski15@yahoo.com>
2023-12-01 09:11:25 +0100

    12/01/23 Zadaca Domasna

---
+++ src/Klasa_2.java
@@ -0,0 +1,8 @@
+
+public class Klasa_2 {
+	public static void main(String[] args) {
+		Klasa_1 dog = new Klasa_1();
+		dog.bark();
+	}
+
+}
---
+++ src/Klasa_3.java
@@ -0,0 +1,11 @@
+
+public class Klasa_3 {
+
+	public static void main(String[] args) {
+		Klasa_1 resi = new Klasa_1();
+		Klasa_1 dog = new Klasa_1();
+		dog.bark();
+		resi.zadaca();
+	}
+
+}
